ab1bce4ec9bde82882a94c86e10d35b7190784f7
Sachin BB <sachinbb@Sachins-MacBook-Air.local>
2024-11-01 16:28:15 +0530

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_e_dot = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = [req.strip() for req in file_obj.readlines()]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+setup(
+    name="ML_PROJECT",
+    version="0.0.1",
+    author="Sachin",
+    author_email="sachinbb7@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
