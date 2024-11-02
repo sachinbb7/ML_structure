515c2fe21c982073ef6f981f2e87775e47a5d509
Sachin BB <sachinbb7@gmail.com>
2024-11-02 17:18:53 +0530

    Added logger and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+LogFile = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path =  os.path.join(os.getcwd(), "logs", LogFile)
+os.makedirs(logs_path, exist_ok= True)
+
+LogFilePath = os.path.join(logs_path, LogFile)
+
+logging.basicConfig(
+
+    filename= LogFilePath,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level= logging.INFO
+
+)
+
+if __name__ == "__main__":
+    logging.info("logging has started")
