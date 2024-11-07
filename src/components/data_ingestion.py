7d534fca8af92fe9b4929fddec130f6c5209141c
Sachin BB <sachinbb7@gmail.com>
2024-11-07 16:12:15 +0530

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,10 @@ from dataclasses import dataclass
 from src.components.data_transform import DataTransformation
 from src.components.data_transform import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 
 
 @dataclass
@@ -53,7 +57,20 @@ if __name__ == "__main__":
 
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr , test_arr , _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+
+    model_trainer = ModelTrainer()
+    r2_score = model_trainer.initiate_model_trainer(train_arr , test_arr)
+    print(r2_score)
+
+
+
+
+
+
+
+
 
         
 
